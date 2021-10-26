1ad2c0ac31bfb042d96e408c4782dab8d6093e51
Alexis Gonzalez <62397939+alexisgnzalez@users.noreply.github.com>
2021-10-26 12:17:19 -0400

    Revert "Index skeleton"

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  # Users routes
   devise_for :users
-
-  # Route to the homepage
   root to: 'offers#home'
-
-  # The rest of the routes
-  resources :offers do
-    resources :bookings, only: %i[new create]
-  end
-  resources :bookings, only: %i[destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
