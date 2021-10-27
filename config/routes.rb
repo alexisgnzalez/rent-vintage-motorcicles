399fcbc44661b010cb853dd6a1efba142e823ac0
Alexis Gonzalez <1991.alexis@gmail.com>
2021-10-27 08:27:45 -0400

    New routes for garage and booked

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   # Route to the homepage
   root to: 'offers#home'
 
+  # Route for booked
+  get 'booked', to: 'bookings#booked', as: :booked
+
+  # Route for garage
+  get 'garage', to: 'offers#garage', as: :garage
+
   # The rest of the routes
   resources :offers do
-    collection do
-      get :garage # creates the route /offers/garage (my own motorcicles)
-    end
     resources :bookings, only: %i[new create edit update]
   end
   resources :bookings, only: %i[destroy]
-  get 'bookings/booked', to: 'bookings#booked'
 end
